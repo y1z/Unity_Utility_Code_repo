1d5a572f9be4be2d4592678fac7c502fbf1c8dba
y1z <whyis2cool@gmail.com>
2024-04-19 00:20:34 -0600

    Add Runtime folder

---
+++ Runtime/SceneLoaderScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Utility
+{
+/// <summary>
+/// This script is used for loading new scenes
+/// </summary>
+    public class SceneLoaderScript : MonoBehaviour
+    {
+        public string sceneName;
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+
+        // scene 
+        public void loadScene()
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
