75d4800aa9185765d7dcaad81d1234dbee0de419
y1z <whyis2cool@gmail.com>
2025-03-15 18:46:26 -0600

    EDebug funcs dont execute without UNITY_EDITOR macro defined.

--- Runtime/EDebug.cs
+++ Runtime/EDebug.cs
@@ -7,75 +7,64 @@ using UnityEngine;
  */
 public static class EDebug
 {
+	[System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void Log(object msg)
     {
-        #if UNITY_EDITOR
         Debug.Log(msg);
-        #endif // UNITY_EDITOR
     }
-
+	
+	[System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void Log(object msg, Object context)
     {
-        #if UNITY_EDITOR
         Debug.Log(msg, context);
-        #endif // UNITY_EDITOR
     }
-
+	
+	[System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void LogError(object msg)
     {
-        #if UNITY_EDITOR
         Debug.LogError(msg);
-        #endif // UNITY_EDITOR
     }
-
+	
+	[System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void LogError(object msg, Object context)
     {
-        #if UNITY_EDITOR
         Debug.LogError(msg, context);
-        #endif // UNITY_EDITOR
     }
 
+	[System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void LogAssertion(object msg, Object context)
     {
-        #if UNITY_EDITOR
         Debug.LogAssertion(msg, context);
-        #endif // UNITY_EDITOR
     }
 
+	[System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void LogAssertion(object msg)
     {
-        #if UNITY_EDITOR
         Debug.LogAssertion(msg);
-        #endif // UNITY_EDITOR
     }
 
+	[System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void Assert(bool condition)
     {
-        #if UNITY_EDITOR
         Debug.Assert(condition);
-        #endif // UNITY_EDITOR
     }
 
-
+	[System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void Assert(bool condition, Object context)
     {
-        #if UNITY_EDITOR
         Debug.Assert(condition, context);
-        #endif // UNITY_EDITOR
     }
 
+	[System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void Assert(bool condition, object message)
     {
-        #if UNITY_EDITOR
         Debug.Assert(condition, message);
-        #endif // UNITY_EDITOR
     }
 
+	[System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void Assert(bool condition, object message, Object context)
     {
-        #if UNITY_EDITOR
         Debug.Assert(condition, message, context);
-        #endif // UNITY_EDITOR
     }
 
 }
