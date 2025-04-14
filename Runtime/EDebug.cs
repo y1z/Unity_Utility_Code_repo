37cdc8be0ed4b1c53e6e5e4584e65b16bf623ae2
y1z <whyis2cool@gmail.com>
2025-04-13 23:35:56 -0600

    EDebug.cs now has a 'EDebug.LogWarning' function

--- Runtime/EDebug.cs
+++ Runtime/EDebug.cs
@@ -7,61 +7,73 @@ using UnityEngine;
  */
 public static class EDebug
 {
-	[System.Diagnostics.Conditional("UNITY_EDITOR")]
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void Log(object msg)
     {
         Debug.Log(msg);
     }
-	
-	[System.Diagnostics.Conditional("UNITY_EDITOR")]
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void Log(object msg, Object context)
     {
         Debug.Log(msg, context);
     }
-	
-	[System.Diagnostics.Conditional("UNITY_EDITOR")]
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogWarning(object message)
+    {
+        Debug.LogWarning(message);
+    }
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogWarning(object message, Object context)
+    {
+        Debug.LogWarning(message, context);
+    }
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void LogError(object msg)
     {
         Debug.LogError(msg);
     }
-	
-	[System.Diagnostics.Conditional("UNITY_EDITOR")]
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void LogError(object msg, Object context)
     {
         Debug.LogError(msg, context);
     }
 
-	[System.Diagnostics.Conditional("UNITY_EDITOR")]
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void LogAssertion(object msg, Object context)
     {
         Debug.LogAssertion(msg, context);
     }
 
-	[System.Diagnostics.Conditional("UNITY_EDITOR")]
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void LogAssertion(object msg)
     {
         Debug.LogAssertion(msg);
     }
 
-	[System.Diagnostics.Conditional("UNITY_EDITOR")]
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void Assert(bool condition)
     {
         Debug.Assert(condition);
     }
 
-	[System.Diagnostics.Conditional("UNITY_EDITOR")]
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void Assert(bool condition, Object context)
     {
         Debug.Assert(condition, context);
     }
 
-	[System.Diagnostics.Conditional("UNITY_EDITOR")]
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void Assert(bool condition, object message)
     {
         Debug.Assert(condition, message);
     }
 
-	[System.Diagnostics.Conditional("UNITY_EDITOR")]
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void Assert(bool condition, object message, Object context)
     {
         Debug.Assert(condition, message, context);
