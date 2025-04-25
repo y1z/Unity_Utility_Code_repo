64a9d900543695b18a349382c782a97e1f2f3fad
y1z <whyis2cool@gmail.com>
2025-04-25 10:53:06 -0600

    Add LogWarningFormat, LogException and AssertFormat

--- Runtime/EDebug.cs
+++ Runtime/EDebug.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /**
- * This class help create debug message that only appear in the editor 
+ * This class help create debug message that only appear in the editor and are removed in the release build 
  */
 public static class EDebug
 {
@@ -32,6 +32,18 @@ public static class EDebug
     }
 
     [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogWarningFormat(string format, params object[] args)
+    {
+        Debug.LogWarningFormat(format, args);
+    }
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogWarningFormat(Object context, string format, params object[] args)
+    {
+        Debug.LogWarningFormat(context, format, args);
+    }
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void LogError(object msg)
     {
         Debug.LogError(msg);
@@ -56,6 +68,18 @@ public static class EDebug
     }
 
     [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogException(System.Exception exception)
+    {
+        Debug.LogException(exception);
+    }
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogException(System.Exception exception, Object context)
+    {
+        Debug.LogException(exception, context);
+    }
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
     public static void Assert(bool condition)
     {
         Debug.Assert(condition);
@@ -77,6 +101,18 @@ public static class EDebug
     public static void Assert(bool condition, object message, Object context)
     {
         Debug.Assert(condition, message, context);
+    }
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void AssertFormat(bool condition, string format, params object[] args)
+    {
+        Debug.AssertFormat(condition, format, args);
+    }
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void AssertFormat(bool condition, Object context, string format, params object[] args)
+    {
+        Debug.AssertFormat(condition, context, format, args);
     }
 
 }
